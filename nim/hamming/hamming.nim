9e7245ed4c4843484295bc9df19d36cc56c3f0df
Krystian Safjan <ksafjan@gmail.com>
2020-04-14 19:34:02 +0200

    Add a.len to range, add comments

--- nim/hamming/hamming.nim
+++ nim/hamming/hamming.nim
@@ -1,11 +1,11 @@
+# Lessons learned:
+# - use len of string
+# - increment varable
+
 proc distance*(a: string, b: string): int =
     if a.len != b.len:
         raise newException(ValueError, "Strings are in different length")
 
-    # tried with:
-    #   for i in 0 .. <a.len:
-    # but doesn't work
-    let l: int = a.len - 1
-    for i in 0 .. l:
+    for i in 0 .. a.len - 1:
         if a[i] != b[i]:
             result.inc
