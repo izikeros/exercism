4ba67794d662501a6e8f6015295fdd85f87a13a5
Krystian Safjan <ksafjan@gmail.com>
2020-04-14 18:57:57 +0200

    Add coments to two-fer solution

--- nim/two-fer/two_fer.nim
+++ nim/two-fer/two_fer.nim
@@ -1,3 +1,6 @@
+# Lessons learned:
+# - string formatting like in f string. You can replace fmt with '&'.
+
 import strformat
 proc twoFer*(name: string ="you"): string =
     result = fmt"One for {name}, one for me."
